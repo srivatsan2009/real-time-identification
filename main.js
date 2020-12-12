2c7b1116a54a44e0c6bec4b00296782c3ec41f6d
srivatsan2009 <71771607+srivatsan2009@users.noreply.github.com>
2020-12-12 11:40:52 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -7,8 +7,24 @@ canvas=createCanvas(300,300);
 canvas.center();
 video=createCapture(VIDEO);
 video.hide();
+ml5_classifier=ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/q4rV8XqYw/model.json",modeloaded);
 } 
+function modeloaded() {
+console.log("model loaded!")
+}
 
 function draw() {
 image(video,0,0,300,300);
+ml5_classifier.classify(video,getresult);
+}
+
+function getresult(error,results) {
+if (error) {
+console.log(error);
+}
+else {
+console.log(results);
+document.getElementById("result_object_name").innerHTML=results[0].label;
+document.getElementById("result_accuracy").innerHTML=results[0].confidence.toFixed(3);
+}
 }
